bb4502c124d669771aee253f99c014848cfc5a31
Santiago <sadoza@gmail.com>
2017-04-21 19:28:39 +0200

    Después de añadir botón y otro formulario

--- WindowsFormsApplication1/Form2.cs
+++ WindowsFormsApplication1/Form2.cs
@@ -10,16 +10,11 @@ using System.Windows.Forms;
 
 namespace WindowsFormsApplication1
 {
-    public partial class Form1 : Form
+    public partial class Form2 : Form
     {
-        public Form1()
+        public Form2()
         {
             InitializeComponent();
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            MessageBox.Show("Has pulsado boton");
         }
     }
 }
